802537af454bc0b2177bf0c221c39081cffb1761
Siddharth <ksid.kaza41@gmail.com>
2024-02-05 15:53:30 -0500

    Fixed bugs in the Scalene triangle. Maintained original logic while not implementing a significant logic change

--- Triangle_1.py
+++ Triangle_1.py
@@ -53,7 +53,7 @@ def classifyTriangle(a,b,c):
         return 'Equilateral'
     elif ((a ** 2) + (b ** 2) == (c ** 2)) or ((a**2)+(c**2)==(b**2)) or ((b**2)+(c**2)==(a**2)): #corrected right angle triangle logic
         return 'Right'
-    elif (a == b) or  (b == c) or (a == c): #Fixed Isoceles triangle logic
-        return 'Isoceles'
-    else:
+    elif (a != b) and  (b != c) and (a != c): #Fixed Isoceles triangle logic
         return 'Scalene'
+    else:
+        return 'Isoceles'
